0653b23fe76132fe601dc51855cd1dd8dbc3aa8b
yangyang <>
2021-07-21 12:02:25 +0800

    update

--- 前端/typescript/study_typescript/src/10、把类作为参数类型的泛型类/index.ts
+++ 前端/typescript/study_typescript/src/10、把类作为参数类型的泛型类/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-15 20:27:55
- * @LastEditTime: 2021-07-15 20:57:22
+ * @LastEditTime: 2021-07-21 11:34:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /study_typescript/src/10、把类作为参数类型的泛型类/index.ts
@@ -35,64 +35,64 @@
 
 // 封装一个操作数据库的库，支持mysql、mssql、mongodb
 // 都有add、update、delete、get方法
-interface DBI<T> {
-  add(info: T): boolean
-  update(info: T, id: number): boolean
-  delete(id: number): boolean
-  get(id: number): any[]
-}
+// interface DBI<T> {
+//   add(info: T): boolean
+//   update(info: T, id: number): boolean
+//   delete(id: number): boolean
+//   get(id: number): any[]
+// }
 
-class MySqlDb<T> implements DBI<T> {
-  add(info: T): boolean {
-    console.log(info)
-    return true
-  }
-  update(info: T, id: number): boolean {
-    throw new Error('Method not implemented.')
-  }
-  delete(id: number): boolean {
-    throw new Error('Method not implemented.')
-  }
-  get(id: number): any[] {
-    throw new Error('Method not implemented.')
-  }
-}
-class MongoDb<T> implements DBI<T> {
-  add(info: T): boolean {
-    throw new Error('Method not implemented.')
-  }
-  update(info: T, id: number): boolean {
-    throw new Error('Method not implemented.')
-  }
-  delete(id: number): boolean {
-    throw new Error('Method not implemented.')
-  }
-  get(id: number): any[] {
-    throw new Error('Method not implemented.')
-  }
-}
-class MsSqlDb<T> implements DBI<T> {
-  add(info: T): boolean {
-    throw new Error('Method not implemented.')
-  }
-  update(info: T, id: number): boolean {
-    throw new Error('Method not implemented.')
-  }
-  delete(id: number): boolean {
-    throw new Error('Method not implemented.')
-  }
-  get(id: number): any[] {
-    throw new Error('Method not implemented.')
-  }
-}
+// class MySqlDb<T> implements DBI<T> {
+//   add(info: T): boolean {
+//     console.log(info)
+//     return true
+//   }
+//   update(info: T, id: number): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   delete(id: number): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   get(id: number): any[] {
+//     throw new Error('Method not implemented.')
+//   }
+// }
+// class MongoDb<T> implements DBI<T> {
+//   add(info: T): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   update(info: T, id: number): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   delete(id: number): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   get(id: number): any[] {
+//     throw new Error('Method not implemented.')
+//   }
+// }
+// class MsSqlDb<T> implements DBI<T> {
+//   add(info: T): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   update(info: T, id: number): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   delete(id: number): boolean {
+//     throw new Error('Method not implemented.')
+//   }
+//   get(id: number): any[] {
+//     throw new Error('Method not implemented.')
+//   }
+// }
 
-class User {
-  username: string | undefined
-  password: string | undefined
-}
-const u = new User()
-u.username = '张三'
-u.password = '123456'
+// class User {
+//   username: string | undefined
+//   password: string | undefined
+// }
+// const u = new User()
+// u.username = '张三'
+// u.password = '123456'
 
-const mysqlDBO = new MySqlDb<User>()
-mysqlDBO.add(u)
+// const mysqlDBO = new MySqlDb<User>()
+// mysqlDBO.add(u)
---
+++ 前端/vue探索/study_vue2.6/rollup.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-21 10:39:31
+ * @LastEditTime: 2021-07-21 10:50:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /study_vue2.6/rollup.config.js
+ */
+export default {}
